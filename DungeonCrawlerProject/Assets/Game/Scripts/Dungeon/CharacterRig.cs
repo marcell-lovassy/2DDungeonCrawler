4feb47e3f5a75abfd2c172d13d4f5e270ec55752
marcell-lovassy <marcell.lovassy@gmail.com>
2022-04-26 22:57:43 +0200

    Create dungeon prototype

---
+++ DungeonCrawlerProject/Assets/Game/Scripts/Dungeon/CharacterRig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Game.Dungeon
+{
+    public class CharacterRig : MonoBehaviour
+    {
+        [SerializeField]
+        float slotDistance;
+
+        [SerializeField]
+        CharacterSlot[] characterSlots = new CharacterSlot[4];
+
+    }
+}
